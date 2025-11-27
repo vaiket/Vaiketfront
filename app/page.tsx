414f5165fc5ee5642f9c0f6675473f45ef064445
Bikas Sahu <astramizen@gmail.com>
2025-11-27 23:08:12 +0530

    Fix: remove Dashboard import from page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,17 @@
-
 import Hero from "../components/Hero";
 import Features from "../components/Features";
-import Dashboard from "../components/Dashboard";
 import Testimonials from "../components/Testimonials";
 import Pricing from "../components/Pricing";
 import FAQ from "../components/FAQ";
 import Footer from "../components/Footer";
-import Header from "../components/Header"
-
+import Header from "../components/Header";
 
 export default function Home() {
   return (
     <>
-    <Header />
-     
-     <Header />
+      <Header />
       <Hero />
       <Features />
-      <Dashboard />
       <Testimonials />
       <Pricing />
       <FAQ />
